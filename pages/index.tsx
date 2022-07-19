15357418de7e366ac4157e71b60862a28c0f6b4a
ndelanhese <nnathanh3@gmail.com>
2022-07-19 07:50:19 -0300

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ import {
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
-import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
