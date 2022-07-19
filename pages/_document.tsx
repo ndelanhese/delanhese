be86350afbfed04181da4c8077cf3f76430a8923
ndelanhese <nnathanh3@gmail.com>
2022-07-18 22:41:26 -0300

    commit to update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
+import React from 'react'
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="pt-BR">
         <Head />
+        <title>Nathan Delanhese | Home</title>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
